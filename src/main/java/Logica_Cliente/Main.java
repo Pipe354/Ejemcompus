5afe1df5019f2ed39f705785f005387c299ae17f
Lenovo <Lenovo@DIAMANTE>
2025-02-21 11:51:02 -0500

    VALIDACIONES

--- src/main/java/Logica_Cliente/Main.java
+++ src/main/java/Logica_Cliente/Main.java
@@ -1,55 +1,201 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package Logica_Cliente;
 
 import Logica_Negocio.Computador;
 import Logica_Negocio.Persona;
 import java.util.Scanner;
 
-/**
- *
- * @author Santiago Lopez
- */
+
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
         // TODO code application logic here
         Scanner scan = new Scanner(System.in);
-        String marca, serial, tamaño, color;
+        String  serial , tamaño, color;
         String nombre, apellido, cedula, direccion;
         Computador objComputador;
         Persona objPersona;
-
+        int rta = 0,conteo;
+        String marca;
+        
         System.out.println("Digite la marca del computador");
         marca = scan.nextLine();
+        
+        rta = HelperValidacion.ValidarVacio(marca);
+        
+        while (rta > 0) {
+            System.out.println("Digite la marca del computador");
+            marca = scan.nextLine();
+            rta = HelperValidacion.ValidarVacio(marca);
+        }
 
-        System.out.println("Digite el serial del computador");
+        conteo = HelperValidacion.ValidarTodo(marca);
+
+        while (conteo != 0) {
+            System.out.println("Digite la marca de la computador");
+            marca = scan.nextLine();
+            conteo = HelperValidacion.ValidarTodo(marca);
+
+        }
+        
+        System.out.println("Digite el serial del  computador");
         serial = scan.nextLine();
+        
+        rta = HelperValidacion.ValidarVacio(serial);
+        while (rta > 0) {
+            System.out.println("Digite  el serial del computador");
+           serial = scan.nextLine();
+            rta = HelperValidacion.ValidarVacio(serial);
+        }
+
+        conteo = HelperValidacion.ValidarTodoSerial(serial);
+
+        while (conteo != 0) {
+            System.out.println("Digite el serial del computador");
+            serial = scan.nextLine();
+            conteo = HelperValidacion.ValidarTodoSerial(serial);
+
+        }
+        
 
         System.out.println("Digite el tamaño del computador");
         tamaño = scan.nextLine();
+        
+        rta = HelperValidacion.ValidarVacio(tamaño);
+        while (rta > 0) {
+            System.out.println("Digite  el tamaño del computador");
+           tamaño = scan.nextLine();
+            rta = HelperValidacion.ValidarVacio(tamaño);
+        }
+
+        conteo = HelperValidacion.ValidarTodo(tamaño);
+
+        while (conteo != 0) {
+            System.out.println("Digite el tamaño del computador");
+            tamaño = scan.nextLine();
+            conteo = HelperValidacion.ValidarTodo(tamaño);
+
+        }
+        
+        rta = HelperValidacion.ValidarVacio(tamaño);
+        while (rta > 0) {
+            System.out.println("Digite el tamaño del computador");
+           tamaño = scan.nextLine();
+            rta = HelperValidacion.ValidarVacio(tamaño);
+        }
+
+        conteo = HelperValidacion.ValidarTodo(tamaño);
+
+        while (conteo != 0) {
+            System.out.println("Digite el tamaño del computador");
+            tamaño = scan.nextLine();
+            conteo = HelperValidacion.ValidarTodo(tamaño);
+
+        }
 
         System.out.println("Digite el color del computador");
         color = scan.nextLine();
         
-        objComputador= new Computador(marca, serial, tamaño, color);
+        rta = HelperValidacion.ValidarVacio(color);
+        while (rta > 0) {
+            System.out.println("Digite el color del computador");
+           color = scan.nextLine();
+            rta = HelperValidacion.ValidarVacio(color);
+        }
+
+        conteo = HelperValidacion.ValidarTodo(color);
+
+        while (conteo != 0) {
+            System.out.println("Digite el color del computador");
+            color = scan.nextLine();
+            conteo = HelperValidacion.ValidarTodo(color);
+
+        }
         
         System.out.println("Digite el nombre de la persona");
         nombre=scan.nextLine();
         
+        rta = HelperValidacion.ValidarVacio(color);
+        while (rta > 0) {
+            System.out.println("Digite el nombre de la persona");
+           nombre = scan.nextLine();
+            rta = HelperValidacion.ValidarVacio(nombre);
+        }
+
+        conteo = HelperValidacion.ValidarTodo(nombre);
+
+        while (conteo != 0) {
+            System.out.println("Digite el nombre de la persona");
+            nombre = scan.nextLine();
+            conteo = HelperValidacion.ValidarTodo(nombre);
+
+        }
+        
+        
         System.out.println("Digite el apellido de la persona");
         apellido=scan.nextLine();
+        
+        rta = HelperValidacion.ValidarVacio(apellido);
+        while (rta > 0) {
+            System.out.println("Digite el  apellido de la persona");
+           apellido = scan.nextLine();
+            rta = HelperValidacion.ValidarVacio(apellido);
+        }
+
+        conteo = HelperValidacion.ValidarTodo(apellido);
+
+        while (conteo != 0) {
+            System.out.println("Digite el apellido de la persona");
+            apellido = scan.nextLine();
+            conteo = HelperValidacion.ValidarTodo(apellido);
+
+        }
+        
         
         System.out.println("Digite la cedula de la persona");
         cedula=scan.nextLine();
         
+         rta = HelperValidacion.ValidarVacio(cedula);
+        while (rta > 0) {
+            System.out.println("Digite la cedula de la persona");
+           cedula = scan.nextLine();
+            rta = HelperValidacion.ValidarVacio(cedula);
+        }
+
+        conteo = HelperValidacion.ValidarTodoLetra(cedula);
+
+        while (conteo != 0) {
+            System.out.println("Digite la cedula de la persona");
+            cedula = scan.nextLine();
+            conteo = HelperValidacion.ValidarTodoLetra(cedula);
+
+        }
+        
         System.out.println("Digite la direccion de la persona");
         direccion=scan.nextLine();
+        
+        
+        rta = HelperValidacion.ValidarVacio(direccion);
+        while (rta > 0) {
+            System.out.println("Digite la direccion de la persona");
+           direccion = scan.nextLine();
+            rta = HelperValidacion.ValidarVacio(direccion);
+        }
+
+        conteo = HelperValidacion.ValidarTodoDireccion(direccion);
+
+        while (conteo != 0) {
+            System.out.println("Digite la direccion de la persona");
+            direccion = scan.nextLine();
+            conteo = HelperValidacion.ValidarTodoDireccion(direccion);
+
+        }
+        
+        
+        objComputador= new Computador(marca, serial, tamaño, color);
+        
+       
         
         objPersona= new Persona(nombre, apellido, cedula, direccion, objComputador); //eestoy asociando
         
--- src/main/java/Logica_Negocio/Computador.java
+++ src/main/java/Logica_Negocio/Computador.java
@@ -1,13 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Logica_Negocio;
 
-/**
- *
- * @author Santiago Lopez
- */
+
 public class Computador {
     
     public String marca,serial,tamaño, color;
